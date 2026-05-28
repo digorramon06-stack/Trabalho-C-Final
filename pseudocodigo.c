3f837bf5224c14eabb6937f9ee3012eaf0430cc8
Igor Ramon <digorramon06@gmail.com>
2026-05-27 21:54:35 -0300

    Add pseudocode for age authorization logic

---
+++ pseudocodigo.c
@@ -0,0 +1,22 @@
+//ENTRADAS:
+//1.Idade
+//2.Autorização
+
+//SAÍDAS: 
+//1. Entrada Liberada ou Negada
+//2. Quantidade de Entradas Liberadas
+//3. Quantidade de Entradas Negadas
+//4. Maior Idade Registrada
+//5. Média das Idades REgistradas
+
+//CÓDIGO:
+//1.Declaração das Váríaveis Tipo: int - idade(vetor de 10), autorizacao, soma(recebe 0), media(recebe 0), liberados(recebe 0), negados(recebe 0), maiorIdade(recebe 0)
+//2.Declaração de Váriaveis Tipo: float - media
+//3.Ler Idade de 10 Participantes
+//4.Armazenar as 10 Idades em um Vetor
+//5.Ler se Tem Autorização ou Não
+//6.Verificar se é Maior de Idade e se tem Autorização
+//7.Contar e Exibir a Quantidade de Entradas Liberadas
+//8.Contar e Exibir a Quantidade de Entradas Liberadas
+//9.Exibir a Soma das Idades Registradas
+//10.Exibir a Média das Idades Registradas
